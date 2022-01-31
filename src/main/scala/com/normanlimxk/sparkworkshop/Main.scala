a70204ca0e3cdd5a0b23254a42de42013f02c80b
frenoid <frenoid@gmail.com>
2022-02-01 02:37:52 +0800

    Add logic in Main.scala to read Customer.csv

--- src/main/scala/com/normanlimxk/sparkworkshop/Main.scala
+++ src/main/scala/com/normanlimxk/sparkworkshop/Main.scala
@@ -1,6 +1,7 @@
 package com.normanlimxk.sparkworkshop
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, types}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructType}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -12,7 +13,27 @@ object Main {
         appName("Norman's Spark Program").
         getOrCreate()
     }
-    val l = spark.sparkContext.parallelize((1 to 10).toList)
-    l.collect().foreach(println)
+
+    val customerSchema = new StructType().
+      add("CustomerID", IntegerType).
+      add("CustomerName", StringType).
+      add("MemCat", StringType).
+      add("Age", IntegerType).
+      add("Gender", StringType).
+      add("AmtSpent", DoubleType).
+      add("Address", StringType).
+      add("City", StringType).
+      add("CountryID", StringType).
+      add("Title", StringType).
+      add("PhoneNo", StringType)
+
+    val customer = spark.
+      read.
+      option("header", "true").
+      schema(customerSchema).
+      csv("src/main/resources/customer.csv")
+
+    customer.show()
+    customer.printSchema()
   }
 }
